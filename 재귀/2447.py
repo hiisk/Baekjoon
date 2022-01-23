74b0bb3848adf18bdc03368c07d975b74a88fb58
hiisk <rkdgustjr10@gmail.com>
2022-01-23 18:03:46 +0900

    2022-01-23

---
+++ 재귀/2447.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+def star(n):
+	if n == 1: return ['*'] 
+		
+	stars = star(n//3) 
+	arr = []
+	
+	for s in stars: arr.append(s*3)
+	for s in stars: arr.append(s+' '*(n//3)+s)
+	for s in stars: arr.append(s*3)
+		
+	return arr
+
+print('\n'.join(star(num)))
