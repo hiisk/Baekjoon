da1534ff1231bdec9809a771edcbb6908e634cbc
hiisk <rkdgustjr10@gmail.com>
2022-04-28 22:43:41 +0900

    2022-04-28

---
+++ Mathematics/[9085] 더하기.py
@@ -0,0 +1,8 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    N = int(sys.stdin.readline())
+    answer = sum(list(map(int, sys.stdin.readline().split())))
+    print(answer)
