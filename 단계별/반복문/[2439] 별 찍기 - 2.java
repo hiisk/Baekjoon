5e21bea45dc1c8648653066bb569abf1707f156d
hiisk <rkdgustjr10@gmail.com>
2022-05-10 23:03:09 +0900

    2022-05-10

---
+++ 단계별/반복문/[2439] 별 찍기 - 2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 1; i < n+1; i++){
+            bw.write(" ".repeat(n-i)+"*".repeat(i)+"\n");
+        }
+        bw.flush();
+    }
+}
