4edfa874df8a3674ed915d8e4a1be97151ab6a22
hiisk <rkdgustjr10@gmail.com>
2022-05-10 23:36:03 +0900

    2022-05-10

---
+++ 단계별/반복문/[10871] X보다 작은 수.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int a = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+   
+        
+        for(int i = 0; i<a; i++){
+        	int value = Integer.parseInt(st.nextToken());
+        	
+        	if (value< target)
+        		bw.write(value+" ");
+        }
+        bw.flush();
+    }
+}
