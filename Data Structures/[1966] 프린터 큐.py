48df231d2a833062b7bafce84f97513754b6a1ba
hiisk <rkdgustjr10@gmail.com>
2022-05-13 23:19:20 +0900

    2022-05-13

---
+++ Data Structures/[1966] 프린터 큐.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+num = int(sys.stdin.readline())
+
+for i in range(num):
+    n, m = map(int, sys.stdin.readline().split())
+    deq = deque(list(map(int, sys.stdin.readline().split())))
+    cnt = 0
+
+    while deq:
+        best = max(deq)  
+        front = deq.popleft()
+        m -= 1
+
+        if best == front:
+            cnt += 1
+            if m < 0:
+                print(cnt)
+                break
+
+        else: 
+            deq.append(front)
+            if m < 0 :
+                m = len(deq) - 1
