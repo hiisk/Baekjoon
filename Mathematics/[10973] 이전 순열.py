8b66ff370bf369bdbcba28e5a31ecce03839c698
hiisk <rkdgustjr10@gmail.com>
2022-05-24 15:19:19 +0900

    2022-05-24

---
+++ Mathematics/[10973] 이전 순열.py
@@ -0,0 +1,13 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(N - 1, 0, -1):
+    if arr[i - 1] > arr[i]:
+        for j in range(N - 1, 0, -1):
+            print(i, j)
+            if arr[i - 1] > arr[j]:
+                arr[i - 1], arr[j] = arr[j], arr[i - 1]
+                arr = arr[:i] + sorted(arr[i:],reverse=True)
+                print(*arr)
+                exit()
+print(-1)
