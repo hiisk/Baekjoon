bfc4eb651b7986a18e36e1c53576aeac37a7283f
hiisk <rkdgustjr10@gmail.com>
2022-05-24 21:57:15 +0900

    2022-05-24

--- Mathematics/[10973] 이전 순열.py
+++ Mathematics/[10973] 이전 순열.py
@@ -4,7 +4,6 @@ arr = list(map(int, input().split()))
 for i in range(N - 1, 0, -1):
     if arr[i - 1] > arr[i]:
         for j in range(N - 1, 0, -1):
-            print(i, j)
             if arr[i - 1] > arr[j]:
                 arr[i - 1], arr[j] = arr[j], arr[i - 1]
                 arr = arr[:i] + sorted(arr[i:],reverse=True)
