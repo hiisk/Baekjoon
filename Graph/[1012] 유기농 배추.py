a4df0408d70bfdc621c09b973b26067b44b0f50e
hiisk <rkdgustjr10@gmail.com>
2022-05-25 22:55:58 +0900

    2022-05-25

---
+++ Graph/[1012] 유기농 배추.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+T = int(sys.stdin.readline())
+count = 0
+
+def bfs(graph, x, y):
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    deq = deque()
+    deq.append((x,y))
+    graph[x][y] = 0
+    
+    global count
+    
+    while deq:
+        x,y = deq.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= M or ny <0 or ny >= N:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                deq.append((nx,ny))
+        if len(deq) == 0:
+            count+=1
+    return count
+
+for _ in range(T):
+    M, N, K = map(int,sys.stdin.readline().split())
+    graph = [[0]*N for _ in range(M)]
+    count = 0
+    for i in range(K):
+        X, Y = map(int,sys.stdin.readline().split())
+        graph[X][Y] = 1
+    for i in range(M):
+        for j in range(N):
+            if graph[i][j]==1:
+                bfs(graph, i, j) 
+    print(count)
