ae6feba4c8e90c7e8f2c3ff2a161360583b953ca
hiisk <rkdgustjr10@gmail.com>
2022-06-04 20:39:50 +0900

    2022-06-04

---
+++ Data Structures/[18258] 큐 2.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+arr = deque()
+
+for i in range(int(sys.stdin.readline())):
+    tmp = list(sys.stdin.readline().split())
+    
+    if tmp[0] == "push":
+        arr.append(tmp[1])
+    elif tmp[0] == "pop":
+        if len(arr) > 0:
+            print(arr.popleft())
+        else:
+            print(-1)
+    elif tmp[0] == "size":
+        print(len(arr))
+    elif tmp[0] == "empty":
+        if len(arr) > 0:
+            print(0)
+        else:
+            print(1)
+    elif tmp[0] == "front":
+        if len(arr) > 0:
+            print(arr[0])
+        else:
+            print(-1)
+    elif tmp[0] == "back":
+        if len(arr) > 0:
+            print(arr[-1])
+        else:
+            print(-1)
