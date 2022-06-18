8dde6a2f4395795639dc92ccc596ebac6f51295e
hiisk <rkdgustjr10@gmail.com>
2022-06-18 11:58:59 +0900

    2022-06-18

---
+++ Data Structures/[5397] 키로거.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+for _ in range(n):
+    left = []
+    right = []
+    pwd = input()
+
+    for x in pwd:
+        if x == ">":
+            if right:
+                left.append(right.pop()) 
+        elif x=="<":
+            if left:
+                right.append(left.pop())
+        elif x=="-":
+            if left:
+                left.pop()
+        else:
+            left.append(x)
+
+    print("".join(left)+"".join(reversed(right)))
