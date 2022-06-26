89b1936bd13ce8e489bcb063b06aa39e0fd100af
hiisk <rkdgustjr10@gmail.com>
2022-06-26 15:58:16 +0900

    2022-06-26

---
+++ Data Structures/[11652] 카드.py
@@ -0,0 +1,13 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+dic = {}
+
+for _ in range(n):
+    tmp = int(sys.stdin.readline().rstrip())
+    if tmp in dic:
+        dic[tmp] +=1
+    else:
+        dic[tmp] = 1
+
+answer = sorted(dic.items(),key = lambda x : (-x[1],x[0]))
+print(answer[0][0])
