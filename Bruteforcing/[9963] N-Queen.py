d26242ff873b06116ea53a742d075d6b26806023
hiisk <rkdgustjr10@gmail.com>
2022-07-24 14:36:14 +0900

    2022-07-24

---
+++ Bruteforcing/[9963] N-Queen.py
@@ -0,0 +1,25 @@
+def dfs(depth):
+    global count
+    if depth == n:
+        count += 1
+        return
+
+    for i in range(n):
+        if visited[i]==0:
+            graph[depth] = i
+            check=True
+            for j in range(depth):
+                if abs(graph[depth]-graph[j])==abs(depth-j):
+                    check=False
+                    break
+            if check:
+                visited[i] = 1
+                dfs(depth+1)
+                visited[i] = 0
+
+n = int(input())
+graph = [0]*n
+visited = [0]*n
+count = 0
+dfs(0)
+print(count)
