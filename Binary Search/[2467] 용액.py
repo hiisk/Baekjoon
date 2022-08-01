085da3018890f93e3692931088367b6c999dbe2a
hiisk <rkdgustjr10@gmail.com>
2022-08-01 19:22:28 +0900

    2022-08-01

---
+++ Binary Search/[2467] 용액.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+liquids = list(map(int, input().rsplit()))
+
+l, r, curr, answer = 0, n-1, 0, sys.maxsize
+ansl, ansr = 0, 0
+
+while l < r:
+    if abs(liquids[l] + liquids[r]) < answer:
+        answer = abs(liquids[l] + liquids[r])
+        ansl, ansr = liquids[l], liquids[r]
+    
+    if liquids[l] + liquids[r] < 0:
+        l += 1
+    else:
+        r -= 1
+
+print(ansl, ansr)
