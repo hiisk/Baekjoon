6bb02b6aac3fb94b9b9ba1f3694a12a130340f89
hiisk <rkdgustjr10@gmail.com>
2022-08-13 12:08:00 +0900

    2022-08-13

---
+++ Graph/[21316] 스피카.py
@@ -0,0 +1,22 @@
+dic = {}
+arr = []
+for _ in range(12):
+	x, y = map(int,input().split())
+	if x in dic:
+		dic[x].append(y)
+	else:
+		dic[x] = [y]
+	if y in dic:
+		dic[y].append(x)
+	else:
+		dic[y] = [x]
+for i in dic:
+	if len(dic[i]) == 3:
+		arr.append(i)
+for i in arr:
+	sum = 0
+	for j in dic[i]:
+		sum += len(dic[j])
+	if sum == 6:
+		print(i)
+		break
