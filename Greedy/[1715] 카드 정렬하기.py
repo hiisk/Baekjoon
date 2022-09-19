25c2c9c3895acdc8b30073e148a5ce4256acfb9d
hiisk <rkdgustjr10@gmail.com>
2022-09-19 11:42:28 +0900

    2022-09-19

---
+++ Greedy/[1715] 카드 정렬하기.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+
+n = int(int(sys.stdin.readline()))
+arr = []
+for _ in range(n):
+    heapq.heappush(arr, int(sys.stdin.readline()))
+
+if len(arr) == 1:
+    print(0)
+else:
+    answer = 0
+    while len(arr) > 1:
+        a = heapq.heappop(arr)
+        b = heapq.heappop(arr)
+        answer += a + b
+        heapq.heappush(arr, a + b)
+    print(answer)
